c43cba731acf2fc47a1b4fcdcf4119d99ea6883c
rikichan <33043738+rikichan@users.noreply.github.com>
2017-10-23 22:27:16 -0300

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import 'hammerjs';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { HomeModule } from './home/home.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule,
+    NgbModule.forRoot(),
+    ServiceWorkerModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule, } from '@angular/common';
+import { BrowserModule  } from '@angular/platform-browser';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+// import { ProfileComponent } from './profile/profile.component';
+// import { SignupComponent } from './signup/signup.component';
+// import { LandingComponent } from './landing/landing.component';
+// import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
+
+const routes: Routes =[
+    { path: '',   redirectTo: '/', pathMatch: 'full' },
+    { path: '',                component: HomeComponent }
+    // { path: 'user-profile',     component: ProfileComponent },
+    // { path: 'signup',           component: SignupComponent },
+    // { path: 'landing',          component: LandingComponent },
+    // { path: 'nucleoicons',      component: NucleoiconsComponent },
+    // { path: '**', redirectTo: 'home'}
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+  ],
+})
+export class AppRoutingModule { }
